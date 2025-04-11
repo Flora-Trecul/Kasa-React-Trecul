8c45dd321d6972c31d712fe4c6ed65802ab3836f
Flora Trecul <flora.trecul@gmail.com>
2025-04-11 21:03:29 +0200

    Implemented collapse animations (div progressive apparition + text sliding)

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import chevronUp from "../assets/icon_chevron_up.png"
 import "../styles/Collapse.scss"
 
@@ -6,6 +6,19 @@ function Collapse({ title, content }) {
 	const [open, setOpen] = useState(false)
 	const alt = open ? "bas" : "haut"
 	const active = open ? " active" : ""
+
+	const [contentHeight, setHeight] = useState("0px")
+	// On crée une référence et on l'associe ensuite au paragraphe dont on veut calculer la hauteur grâce à l'attribut ref
+	const contentRef = useRef(null)
+
+	// Actualise la hauteur à chaque fois que l'état de la collpase change
+	// La propriété .current est spécifique au hook useEffect
+	// La propriété scrollHeigth permet de mesurer la hauteur en pixels d'un élément
+	useEffect(() => {
+		// La valeur height générée est trop grande, on récupère le padding-bottom pour le soustraire
+		const padding = parseFloat(getComputedStyle(contentRef.current).paddingBottom)
+		open ? setHeight(`${contentRef.current.scrollHeight - padding}px`) : setHeight("0px")
+	}, [open])
 
 	return (
 		<>
@@ -16,7 +29,9 @@ function Collapse({ title, content }) {
 				src={chevronUp}
 				alt={`Icône chevron vers le ${alt}`}
 			/>
-			<p className={"collapse__content" + active}>{content}</p>
+			<div ref={contentRef} style={{ height: contentHeight }} className={`collapse__content${open ? " active" : ""}`}>
+				<p className="collapse__content slide">{content}</p>
+			</div>
 		</>
 	)
 }
