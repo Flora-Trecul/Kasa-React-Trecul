8b8d21c721382ebacbd2d70f6fcceef0e114e6bc
Flora Trecul <flora.trecul@gmail.com>
2025-04-12 14:51:27 +0200

    Implemented slideshow animation

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,21 @@
 import { useLocation } from "react-router-dom"
 import "../styles/Banner.scss"
 
-function Banner({ section, src, desc, title }) {
+function Banner({ section, src, desc, title, direction, srcNext, srcPrevious }) {
 	const path = useLocation().pathname
+	const slider = path.startsWith("/lodging") ? " slider" + direction : ""
+
 	return (
-		<div className={section + "__banner"}>
+		<div className={section + "__banner" + slider}>
+			{path.startsWith("/lodging") && (
+				<img className="banner__img" alt="Image précédente dans le carrousel" src={srcPrevious} />
+			)}
 			<img src={src} alt={`Bannière : ${desc}`} className="banner__img" />
-			{!path.startsWith("/lodging") && <div className="banner__overlay"></div>}
+			{path.startsWith("/lodging") ? (
+				<img className="banner__img" alt="Image suivante dans le carrousel" src={srcNext} />
+			) : (
+				<div className="banner__overlay"></div>
+			)}
 			{path === "/" && <h2 className="home__title">{title}</h2>}
 		</div>
 	)
--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -6,20 +6,57 @@ import "../styles/Slideshow.scss"
 
 function Slideshow({ gallery, title }) {
 	const [imgSrc, updateSrc] = useState(0)
+	const [srcNext, updateNext] = useState(1)
+	const [srcPrevious, updatePrevious] = useState(gallery.length - 1)
+	const [direction, setDirection] = useState("")
 
 	function handleClick(e) {
 		const arrow = e.target.ariaLabel
 
-		if (arrow === "Previous") {
-			updateSrc(imgSrc - 1 < 0 ? gallery.length - 1 : imgSrc - 1)
-		} else {
-			updateSrc(imgSrc + 1 >= gallery.length ? 0 : imgSrc + 1)
-		}
+		setDirection(arrow === "Previous" ? " slide-previous" : " slide-next")
+
+		setTimeout(() => {
+			updateSrc(
+				arrow === "Previous"
+					? imgSrc - 1 < 0
+						? gallery.length - 1
+						: imgSrc - 1
+					: imgSrc + 1 >= gallery.length
+						? 0
+						: imgSrc + 1
+			)
+			updateNext(
+				arrow === "Previous"
+					? srcNext - 1 < 0
+						? gallery.length - 1
+						: srcNext - 1
+					: srcNext + 1 >= gallery.length
+						? 0
+						: srcNext + 1
+			)
+			updatePrevious(
+				arrow === "Previous"
+					? srcPrevious - 1 < 0
+						? gallery.length - 1
+						: srcPrevious - 1
+					: srcPrevious + 1 >= gallery.length
+						? 0
+						: srcPrevious + 1
+			)
+			setDirection("")
+		}, 1000)
 	}
 
 	return (
 		<div className="banner slideshow">
-			<Banner section="lodging" src={gallery[imgSrc]} desc={`Photo de l'hébergement ${title}`} />
+			<Banner
+				section="lodging"
+				src={gallery[imgSrc]}
+				srcNext={gallery[srcNext]}
+				srcPrevious={gallery[srcPrevious]}
+				desc={`Photo de l'hébergement ${title}`}
+				direction={direction}
+			/>
 			{gallery.length > 1 && (
 				<>
 					<img
